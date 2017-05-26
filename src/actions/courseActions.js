a227ca591a9eb2d52f61689c949d75a236d73ac2
[manish chandra] <[manishchandrakumar@gmail.com]>
2017-05-26 13:56:08 +0530

    formatting

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,7 +1,6 @@
 import * as types from './actionTypes';
 
 export function createCourse(course) {
-  // Action Creator: "CREATE_COURSE"
-  // Action: "course"
+  // Action Creator: "CREATE_COURSE" Action: "course"
   return {type: types.CREATE_COURSE, course};
 }
