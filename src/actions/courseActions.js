0e7eb0afb60cf3e61fb2313380e3ba1171622cf2
[manish chandra] <[manishchandrakumar@gmail.com]>
2017-05-26 13:50:53 +0530

    added action types as a constant, in seprate file

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,7 @@
+import * as types from './actionTypes';
+
 export function createCourse(course) {
   // Action Creator: "CREATE_COURSE"
   // Action: "course"
-  return {type: 'CREATE_COURSE', course};
+  return {type: types.CREATE_COURSE, course};
 }
